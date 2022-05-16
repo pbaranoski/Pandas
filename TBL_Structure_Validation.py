29ac9702a2f6e67d4aa358900fb0f91ddf3cccce
pbaranoski <94907262+pbaranoski@users.noreply.github.com>
2022-05-16 11:30:27 -0400

    Add files via upload

--- TBL_Structure_Validation.py
+++ TBL_Structure_Validation.py
@@ -10,14 +10,22 @@ from pymysql import ROWID
 #TBL_COLS1_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\Snowflake\Compare\BIA_DEV_INFO.csv"
 #TBL_COLS2_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\Snowflake\Compare\BIA_TST_INFO.csv"
 
-TBL_COLS1_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\Snowflake\TST_CLM_CYQ_SGNTR_TEMP.csv"
-TBL_COLS2_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\Teradata\TST_V1_CLM_CYQ_SGNTR.csv"
+#TBL_COLS1_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\Snowflake\TST_CLM_CYQ_SGNTR_TEMP.csv"
+#TBL_COLS2_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\Teradata\TST_V1_CLM_CYQ_SGNTR.csv"
+
+#TBL_COLS1_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\Snowflake\TST_CLM_PTB_PROC_CNTL.csv"
+#TBL_COLS2_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\Teradata\TST_V1_CLM_PTB_PROC_CNTL.csv"
+
+TBL_COLS1_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\FMR\TestResults\JIRA Test results\TST_CLM_FMR_CARR.csv"
+TBL_COLS2_csv = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\FMR\TestResults\JIRA Test results\TST_v1_CLM_FMR_CARR.csv"
 
 #fDtlDiffs = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\Snowflake\Compare\TBL_COL_Diffs.csv"
 #fDtlDiffsXLSX = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\Snowflake\Compare\TBL_COL_Diffs.xlsx"
-fDtlDiffs = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\CLM_CYQ_SGNTR_Diffs.csv"
-fDtlDiffsXLSX = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\CLM_CYQ_SGNTR_Diffs.xlsx"
+#fDtlDiffs = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\CLM_CYQ_SGNTR_Diffs.csv"
+#fDtlDiffsXLSX = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\Historical\CLM_CYQ_SGNTR_Diffs.xlsx"
 
+fDtlDiffs = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\FMR\TestResults\JIRA Test results\TestCLM_FMR_CARR_Diffs.csv"
+fDtlDiffsXLSX = r"C:\Users\user\OneDrive - Apprio, Inc\Documents\PBAR\FMR\TestResults\JIRA Test results\TestCLM_FMR_CARR_Diffs.xlsx"
 
 def removeDFCols(df, lstCols2Remove):
     # sColTxt = Column to drop from Data Frame
@@ -76,35 +84,39 @@ def main():
     print("NOF dfFile2 rows:"+str(len(dfFile2.index)))
 
     #########################################################################################
-    # in essence a join/comparison on all columns
+    # In essence a join/comparison on all columns
     # NOTE: Setting "indicator=True" adds a column to the merged DataFame where the value 
     #       of each row can be one of three possible values: left_only, right_only, or both
     # NOTE2: Another option "first_df.compare(second_df, keep_equal=True)"
     #########################################################################################
     comparison_df = dfFile1.merge(dfFile2, indicator=True, how='outer')
     comparison_df = sortDF(comparison_df)
-    comparison_df.to_csv(fDtlDiffs,sep = ",", index=False, line_terminator = "\n")  
+
+    # All Comparisons
+    ##comparison_df.to_csv(fDtlDiffs,sep = ",", index=False, line_terminator = "\n")  
 
     # after merge --> row will be "marked" as 'both" if both data frame rows match
     dfDiff = comparison_df[comparison_df['_merge'] != 'both']
     dfDiff = sortDF(dfDiff)
 
-    exit(0)
-    # sort and write out results    
-    
-    ##dfDiffSorted.to_csv(fDtlDiffs,sep = ",", index=False, line_terminator = "\n")
-    dfDiff.to_csv(fDtlDiffs,sep = ",", index=False, line_terminator = "\n")    
-    
-    exit(0)
+    dfMatches = comparison_df[comparison_df['_merge'] == 'both']
+    dfMatches = sortDF(dfMatches)
 
-    # saving diffs to xlsx file
-    XLSX = pd.ExcelWriter(fDtlDiffsXLSX)
-    dfDiffSorted.to_excel(XLSX, index=False)    
-    XLSX.save()
+    ##########################################
+    # saving Results to xlsx file
+    ##########################################
+    XLSXWriter = pd.ExcelWriter(fDtlDiffsXLSX)
+
+    dfDiff.to_excel(XLSXWriter, sheet_name="Differences", index=False)   
+    dfMatches.to_excel(XLSXWriter, sheet_name="Matches", index=False)   
+
+    XLSXWriter.save()
 
 
-    ###\/######
+    ################################
+    # Modify Excel spreadsheet
     # Add hilighting of rows
+    ################################
     wrkbk = excel.load_workbook(fDtlDiffsXLSX)
 
     if wrkbk is None:
@@ -115,34 +127,45 @@ def main():
     ###################################################
     # process Excel spreadsheet
     ###################################################
-    bFlipColor = False
+    for sheet in wrkbk:
 
-    sheet = wrkbk.active
-    rowColor = "FFFF00"
+        thin = Side(border_style="thin")
+        cellBorder = Border(top=thin, left=thin, right=thin, bottom=thin)
 
-    for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row, min_col=1, max_col=sheet.max_column):
+        sheet.freeze_panes = 'A2'
 
-        if row[0].row == 1:
-            pass
-            # skip header
-        elif (row[0].row) % 2 == 0:
-            if bFlipColor == False:
-                bFlipColor = True
-                rowColor = 'B0FFFF'
-            else:
-                bFlipColor = False
-                rowColor = '0DFFBE'  
+        bFlipColor = False
 
+        #sheet = wrkbk.active
+        # Default rowColor
+        rowColor = "FFFF00"
 
-        for cell in row:
-            if cell.row == 1:
-                # color header row
-                cell.fill = PatternFill(start_color='66DDFF', end_color='66DDFF', fill_type = "solid")                
-            else:
-                cell.fill = PatternFill(start_color=rowColor, end_color=rowColor, fill_type = "solid")
-                                        
+        for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row, min_col=1, max_col=sheet.max_column):
+
+            # Set Row Color
+            if row[0].row == 1:
+                pass
+                # skip header
+            elif (row[0].row) % 2 == 0:
+                if bFlipColor == False:
+                    bFlipColor = True
+                    rowColor = 'DDEBF6'
+                else:
+                    bFlipColor = False
+                    rowColor = 'E2EFDB'  
+
+            # Color cells with Current RowColor
+            for cell in row:
+                if cell.row == 1:
+                    # color header row
+                    cell.fill = PatternFill(start_color='01B0F1', end_color='01B0F1', fill_type = "solid")                
+                    cell.border = cellBorder
+                else:
+                    cell.fill = PatternFill(start_color=rowColor, end_color=rowColor, fill_type = "solid")
+                    cell.border = cellBorder
+
+    # Save workbook changes                                            
     wrkbk.save(fDtlDiffsXLSX)
-
   
     exit(0)
 
